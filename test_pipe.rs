e091558e4caad79bc0df7989d6b25c3b98369baa
detoro <meanstack20@gmail.com>
2026-02-22 10:37:06 +0700

    chore: Delete `test_pingora.rs` and `test_pipe.rs` files.

--- test_pipe.rs
+++
@@ -1,6 +0,0 @@
-#[tokio::main]
-async fn main() {
-    let mut pipe = redis::pipe();
-    // pipe.set_ex("key", "val", 60); // Check if this exists
-    pipe.cmd("SETEX").arg("key").arg(60).arg("val");
-}
